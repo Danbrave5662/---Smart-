8e6104dd2c7b694d994d9d259ba1f3123131d4eb
Danbrave5662 <danbrave134@gmail.com>
2026-05-22 13:18:29 +0300

    Feat: Додано метод розширення ToMetersPerSecond для типу double для конвертації швидкості.

--- Проєкт/TrafficExtensions.cs
+++ Проєкт/TrafficExtensions.cs
@@ -16,4 +16,11 @@ public static class TrafficExtensions
         // Базова перевірка: номер зазвичай має від 4 до 8 символі
         return plate.Length >= 4 && plate.Length <= 8;
     }
+
+    // Метод розширення для double
+    // Перевод швидкості з км/год на м/c
+    public static double ToMetersPerSecond(this double speedKmh)
+    {
+        return speedKmh * 0.277778;
+    }
 }
