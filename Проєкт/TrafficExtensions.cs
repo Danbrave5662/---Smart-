d02c02be852605245a01ed32c142ab9ccc7f74db
Danbrave5662 <danbrave134@gmail.com>
2026-05-22 13:14:38 +0300

    Feat: Додано статичний клас TrafficExtensions та метод розширення IsValidLicensePlate для типу string.

---
+++ Проєкт/TrafficExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TrafficMonitoringSystem;
+
+public static class TrafficExtensions
+{
+    //  Метод розширення для стандартного типу string
+
+    public static bool IsValidLicensePlate(this string plate)
+    {
+        if (string.IsNullOrWhiteSpace(plate))
+        {
+            return false;
+        }
+
+        // Базова перевірка: номер зазвичай має від 4 до 8 символі
+        return plate.Length >= 4 && plate.Length <= 8;
+    }
+}
